fc8dd092f115eaf06f848d4f53fa3c7cf8e5c6f0
KarittaGam <98600558+KarittaGam@users.noreply.github.com>
2022-01-28 21:50:19 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/karitta/my_first_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.karitta.my_first_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const MyHomePage(title: 'แอปบวกลบปุ่ม'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = _counter + 1;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter = _counter - 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('แอปบวกลบปุ่มนะ'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text('บวก'),
+            ),
+            const Text(
+              'ฉันกดปุ่มนี้ไปแล้ว:',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.teal,
+              ),
+            ),
+            Text(
+              '$_counter',
+              style: const TextStyle(
+                fontSize: 100.0,
+                color: Colors.amber,
+              ),
+            ),
+            const Text(
+              'ครั้ง',
+              style: TextStyle(
+                fontSize: 50.0,
+                color: Colors.teal,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _decrementCounter,
+              child: const Text('ลบ'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
